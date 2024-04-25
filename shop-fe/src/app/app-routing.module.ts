29657667b88d82e5419ccc1e566c5be8ab886c4e
roman <romanpz051@gmail.com>
2024-04-25 19:36:39 +0200

    shop v0.3.0 produc_fe

--- shop-fe/src/app/app-routing.module.ts
+++ shop-fe/src/app/app-routing.module.ts
@@ -2,8 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/auth/components/login/login.component';
 
-const routes: Routes = [{ path: 'logowanie', component: LoginComponent }];
-
+//const routes: Routes = [{ path: 'logowanie', component: LoginComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'produkty',
+    loadChildren: () =>
+      import('./modules/products/products.module').then(
+        (m) => m.ProductsModule,
+      ),
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
--- shop-fe/src/app/modules/core/components/header/header.component.ts
+++ shop-fe/src/app/modules/core/components/header/header.component.ts
@@ -5,6 +5,7 @@ import * as AuthActions from '../../../auth/store/auth.actions';
 import { Observable } from 'rxjs';
 import { User } from '../../models/auth.model';
 import { selectAuthUser } from '../../../auth/store/auth.selectors';
+import { Category } from '../../models/categories.model';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -12,6 +13,12 @@ import { selectAuthUser } from '../../../auth/store/auth.selectors';
 })
 export class HeaderComponent {
   user$: Observable<User | null> = this.store.select(selectAuthUser);
+
+  categories: Category[] = [
+    { name: 'Kategoria1', shortId: 123456 },
+    { name: 'Kategoria2', shortId: 123333 },
+    { name: 'Kategoria3', shortId: 122226 },
+  ];
   constructor(private store: Store<AppState>) {}
   logout() {
     this.store.dispatch(AuthActions.logout());
